dbc9834d7b836a64d869a356f2ed23ec4628df4b
marcelluseasley <marcellus.easley@gmail.com>
2023-03-06 20:41:09 -0500

    go fmt

--- main.go
+++ main.go
@@ -9,20 +9,19 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/sony/gobreaker"
-) 
+)
 
 var startTime time.Time = time.Now()
 
 func main() {
-	
 	go server()
-	
+
 	cb := gobreaker.NewCircuitBreaker(
 		gobreaker.Settings{
-			Name: "my-circuit-breaker",
+			Name:        "my-circuit-breaker",
 			MaxRequests: 5,
-			Timeout: 3 * time.Second,
-			Interval: 1 * time.Second,
+			Timeout:     3 * time.Second,
+			Interval:    1 * time.Second,
 			ReadyToTrip: func(counts gobreaker.Counts) bool {
 				return counts.ConsecutiveFailures > 3
 			},
@@ -43,24 +42,22 @@ func main() {
 		}
 		time.Sleep(100 * time.Millisecond)
 	}
-    
 }
 
 func server() {
-	
 	app := fiber.New()
 	app.Use(logger.New())
 
-    app.Get("/ping", func(c *fiber.Ctx) error {
-        if time.Since(startTime) < 5*time.Second {
+	app.Get("/ping", func(c *fiber.Ctx) error {
+		if time.Since(startTime) < 5*time.Second {
 			return fiber.NewError(fiber.StatusInternalServerError, "pong")
 		}
 		c.SendStatus(fiber.StatusOK)
 		return c.SendString("pong")
-    })
+	})
 
 	fmt.Println("Starting server at port 8080")
-    app.Listen(":3000")
+	app.Listen(":3000")
 }
 
 func DoReq() error {
@@ -68,11 +65,11 @@ func DoReq() error {
 	req := a.Request()
 	req.Header.SetMethod(fiber.MethodGet)
 	req.SetRequestURI("http://127.0.0.1:3000/ping")
-	
+
 	if err := a.Parse(); err != nil {
 		panic(err)
 	}
-	
+
 	code, body, errs := a.String()
 	if len(errs) > 0 {
 		for _, err := range errs {
@@ -80,11 +77,10 @@ func DoReq() error {
 		}
 		return errors.New("bad response")
 	}
-	
+
 	if code < 200 || code >= 300 {
 		return errors.New("bad response")
 	}
 	fmt.Println(body)
 	return nil
-
 }
